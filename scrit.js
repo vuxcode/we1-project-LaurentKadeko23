b418b6db3247939ba7bf512efcd439d2dfe3e7cc
Laurent Kadeko <laka020402@skovde.se>
2024-02-09 19:49:58 +0100

    small interactions
    Whenever you hoverr over  the h2 it will get bigger and when you leave it get back to it's normal size

---
+++ scrit.js
@@ -0,0 +1,9 @@
+// wheneveer I hover over the ingridients it will get bigger upp to 200% and when Im not point at it it will get back to norlam
+
+function ingridientsHover(){
+  document.getElementById("ingridients").firstElementChild.style.fontSize = "200%"
+}
+
+function ingridientsNormal(){
+  document.getElementById("ingridients").firstElementChild.style.fontSize = "100%"
+}
